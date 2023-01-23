1d4e9f125d2157b60aa15ee210a093a10480e53e
Rishish <rishish.singappan@gmail.com>
2023-01-23 20:38:56 +1030

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Windows;
+
+namespace Userauthentication
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            DatabaseFacade facade = new DatabaseFacade(new userdatacontext());
+            facade.EnsureCreated();
+        }
+    }
+}
---
+++ userdatacontext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.Data.Sqlite;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Userauthentication
+{
+    public class userdatacontext : DbContext
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("DataSource = DataFile.db");
+        }
+
+       public DbSet<user> Users { get; set; }
+
+        
+    }
+
+
+}
+    
